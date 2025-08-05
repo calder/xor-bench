795a767067c29952cdcf3d176e45d27a3951251a
Calder Coalson <caldercoalson@gmail.com>
2025-08-05 14:15:00 -0700

    Add raw u32 array xor.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,14 @@ use std::time::SystemTime;
 
 use bitvec::prelude::*;
 
+fn xor_u32(a: &[u32], b: &[u32], c: &mut [u32]) {
+    assert_eq!(a.len(), b.len());
+    assert_eq!(a.len(), c.len());
+    for i in 0..a.len() {
+        c[i] = a[i] ^ b[i];
+    }
+}
+
 fn bitvec_xor1(a: &BitSlice, b: &BitSlice) -> BitVec {
     assert_eq!(a.len(), b.len());
     let mut c = a.to_bitvec();
@@ -17,18 +25,24 @@ fn bitvec_xor2(a: &BitSlice, b: &BitSlice, c: &mut BitSlice) {
     c.bitxor_assign(b);
 }
 
-fn bitvec_xor3(a: &BitSlice, b: &BitSlice, c: &mut BitSlice) {
-    assert_eq!(a.len(), b.len());
-    assert_eq!(a.len(), c.len());
-    for i in 0..a.len() {
-        c.set(i, a[i] ^ b[i]);
-    }
-}
-
 fn main() {
     const SAMPLES: usize = 10;
     const ITERS: usize = 10000;
     const LEN: usize = 4096;
+
+    let a = [0; LEN];
+    let b = [0; LEN];
+    let mut c = [0; LEN];
+
+    println!("xor_u32:");
+    for _ in 0..SAMPLES {
+        let start = SystemTime::now();
+        for _ in 0..ITERS {
+            black_box(xor_u32(black_box(&a), black_box(&b), black_box(&mut c)));
+        }
+        let time_us = 1e6 * start.elapsed().unwrap().as_secs_f32() / ITERS as f32;
+        println!("{:8.2} us", time_us);
+    }
 
     let a = bitvec![0; LEN];
     let b = bitvec![0; LEN];
@@ -49,16 +63,6 @@ fn main() {
         let start = SystemTime::now();
         for _ in 0..ITERS {
             black_box(bitvec_xor2(black_box(&a), black_box(&b), black_box(&mut c)));
-        }
-        let time_us = 1e6 * start.elapsed().unwrap().as_secs_f32() / ITERS as f32;
-        println!("{:8.2} us", time_us);
-    }
-
-    println!("bitvec_xor3:");
-    for _ in 0..SAMPLES {
-        let start = SystemTime::now();
-        for _ in 0..ITERS {
-            black_box(bitvec_xor3(black_box(&a), black_box(&b), black_box(&mut c)));
         }
         let time_us = 1e6 * start.elapsed().unwrap().as_secs_f32() / ITERS as f32;
         println!("{:8.2} us", time_us);
