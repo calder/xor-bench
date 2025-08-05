18d4af33a507f35ddde4e04857586e76f16fbaf2
Calder Coalson <caldercoalson@gmail.com>
2025-08-05 14:16:55 -0700

    Add raw u64 array xor.

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,14 @@ fn xor_u32(a: &[u32], b: &[u32], c: &mut [u32]) {
     }
 }
 
+fn xor_u64(a: &[u64], b: &[u64], c: &mut [u64]) {
+    assert_eq!(a.len(), b.len());
+    assert_eq!(a.len(), c.len());
+    for i in 0..a.len() {
+        c[i] = a[i] ^ b[i];
+    }
+}
+
 fn bitvec_xor1(a: &BitSlice, b: &BitSlice) -> BitVec {
     assert_eq!(a.len(), b.len());
     let mut c = a.to_bitvec();
@@ -44,9 +52,23 @@ fn main() {
         println!("{:8.2} us", time_us);
     }
 
-    let a = bitvec![0; LEN];
-    let b = bitvec![0; LEN];
-    let mut c = bitvec![0; LEN];
+    let a = [0; LEN / 2];
+    let b = [0; LEN / 2];
+    let mut c = [0; LEN / 2];
+
+    println!("xor_u64:");
+    for _ in 0..SAMPLES {
+        let start = SystemTime::now();
+        for _ in 0..ITERS {
+            black_box(xor_u64(black_box(&a), black_box(&b), black_box(&mut c)));
+        }
+        let time_us = 1e6 * start.elapsed().unwrap().as_secs_f32() / ITERS as f32;
+        println!("{:8.2} us", time_us);
+    }
+
+    let a = bitvec![0; LEN * 32];
+    let b = bitvec![0; LEN * 32];
+    let mut c = bitvec![0; LEN * 32];
 
     println!("bitvec_xor1:");
     for _ in 0..SAMPLES {
