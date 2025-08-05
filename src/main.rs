124bdd00a1e131b9142c238175858cdc0f09ffdd
Calder Coalson <caldercoalson@gmail.com>
2025-08-05 14:13:04 -0700

    Add raw iteration.

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,16 @@ fn bitvec_xor1(a: &BitSlice, b: &BitSlice) -> BitVec {
 
 fn bitvec_xor2(a: &BitSlice, b: &BitSlice, c: &mut BitSlice) {
     assert_eq!(a.len(), b.len());
+    assert_eq!(a.len(), c.len());
     c.bitxor_assign(b);
+}
+
+fn bitvec_xor3(a: &BitSlice, b: &BitSlice, c: &mut BitSlice) {
+    assert_eq!(a.len(), b.len());
+    assert_eq!(a.len(), c.len());
+    for i in 0..a.len() {
+        c.set(i, a[i] ^ b[i]);
+    }
 }
 
 fn main() {
@@ -32,7 +41,7 @@ fn main() {
             black_box(bitvec_xor1(black_box(&a), black_box(&b)));
         }
         let time_us = 1e6 * start.elapsed().unwrap().as_secs_f32() / ITERS as f32;
-        println!("    {:8.2} us", time_us);
+        println!("{:8.2} us", time_us);
     }
 
     println!("bitvec_xor2:");
@@ -42,6 +51,16 @@ fn main() {
             black_box(bitvec_xor2(black_box(&a), black_box(&b), black_box(&mut c)));
         }
         let time_us = 1e6 * start.elapsed().unwrap().as_secs_f32() / ITERS as f32;
-        println!("    {:8.2} us", time_us);
+        println!("{:8.2} us", time_us);
+    }
+
+    println!("bitvec_xor3:");
+    for _ in 0..SAMPLES {
+        let start = SystemTime::now();
+        for _ in 0..ITERS {
+            black_box(bitvec_xor3(black_box(&a), black_box(&b), black_box(&mut c)));
+        }
+        let time_us = 1e6 * start.elapsed().unwrap().as_secs_f32() / ITERS as f32;
+        println!("{:8.2} us", time_us);
     }
 }
